2818d3f24fa9da32d4840a0787968c2e8e12110e
Joel-leal <joel.lasi@hotmail.com>
2021-02-03 20:15:57 -0300

    melhoria exm1

--- exm1.js
+++ exm1.js
@@ -8,20 +8,6 @@
 //* Caso tenhamos mais de uma palavra com o número maior de letras, retorne a primeira palavra encontrada
 //* Se a lista for vazia, retorne null
 
-// const list = ["ana", "paulo", "enzo", "pedro", "rafaela"]
-// console.log(list.sort()) > Ordena os nomes na array
-// let maiorPalavra = list[0]
- 
-// for (let i = 0; i < list.length; i++){
-//     if(maiorPalavra.length < list[i].length){
-        
-//         maiorPalavra = list[i]
-//     }
-//    console.log(i+" "+maiorPalavra)
-//     console.log(`${i} ${maiorPalavra}`)
-// }
-// console.log(maiorPalavra)
-
 function pegaMaiorTamannhoPalavra(list){
  
     let maiorPalavra = list[0]
@@ -30,11 +16,9 @@ function pegaMaiorTamannhoPalavra(list){
            
             maiorPalavra = list[i]
         }
-       //console.log(i+" "+maiorPalavra) 
     }
     return maiorPalavra
 }
-
 const list1 = ["paulo", "pedro", "ana"]
 const list2 = ["ana", "paulo", "enzo", "pedro", "rafaela"]
 
