e1313660676231ea5bc272c1246947e47edce458
joel-morais <joel.lasi@hotmail.com>
2021-06-09 18:11:38 -0300

    chamando endereço pelo CEP

--- API/cep.js
+++ API/cep.js
@@ -1,21 +1,28 @@
-const cep = 31155555550;
+const selectCep = document.getElementById("input-cep");
 
-async function getCep(cep) {
-  const url = `https://viacep.com.br/ws/${cep}/json/`;
-  //   const res = await fetch(url);
-  //   const data = await res.json();
-  //   console.log(data.logradouro);
-
+selectCep.addEventListener("blur", () => {
+  const url = `https://viacep.com.br/ws/${selectCep.value}/json/`;
   fetch(url)
     .then((res) => res.json()) // transforma response em objeto
     .then((data) => {
-      const output = document.getElementById("container");
-      output.innerHTML = JSON.stringify(data, null, 4);
+      const output = document.getElementById("logradouro");
+      output.innerHTML = data.logradouro;
+      const outputBairro = document.getElementById("bairro");
+      outputBairro.innerHTML = data.bairro;
+      const outputLocalidade = document.getElementById("localidade");
+      outputLocalidade.innerHTML = data.localidade;
+      const outputUf = document.getElementById("uf");
+      outputUf.innerHTML = data.uf;
+      //   output.innerHTML = JSON.stringify(data, null, 4);
     })
     .catch((error) => {
-      const output = document.getElementById("container");
+      const output = document.getElementById("logradouro");
       output.innerHTML = error.massage;
     });
-}
+});
 
-getCep(cep);
+// async function getCep(cep) {
+//   //   const res = await fetch(url);
+//   //   const data = await res.json();
+//   //   console.log(data.logradouro);
+// }
