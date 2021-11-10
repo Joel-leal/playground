75667aa5925ba91aadf3d6f790ddb0a91a836c2e
joel leal <joel.lasi@hotmail.com>
2021-11-10 15:27:16 -0300

    formatando os enuciados

--- exerc-logic43.js
+++ exerc-logic43.js
@@ -1,7 +1,7 @@
 //lista de exercícios do dia 4.3
 
-//Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 ,
-//imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+// 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 ,
+// imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
 
 // let n = 10
 // let symbol = '*'
@@ -13,12 +13,14 @@
 //   console.log(listSymbol)
 // }
 
-//Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+// 2-Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
 
-let n = 5
-let symbol = '*'
-let listSymbol = ''
-for(let i = 0; i < 5; i++){
-  listSymbol += symbol
-  console.log(listSymbol)
-}
+// let n = 5
+// let symbol = '*'
+// let listSymbol = ''
+// for(let i = 0; i < 5; i++){
+//   listSymbol += symbol
+//   console.log(listSymbol)
+// }
+
+// 3- Agora inverta o lado do triângulo.
