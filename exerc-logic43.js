48b8d05bacb280b3b876273f026f4e2744ebd2de
joel leal <joel.lasi@hotmail.com>
2021-11-10 15:14:02 -0300

    exercícise 4.3.1

---
+++ exerc-logic43.js
@@ -0,0 +1,14 @@
+//lista de exercícios do dia 4.3
+
+//Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1,
+//imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+
+let n = 10
+let symbol = '*'
+let listSymbol = ''
+for(let i = 0; i < 10; i++){
+  listSymbol += symbol
+}
+for(let k = 0; k < 10; k++){
+  console.log(listSymbol)
+}
