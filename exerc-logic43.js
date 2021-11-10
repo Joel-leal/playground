4de014f5af17d65e93fb8e089762b299c58c9a2c
joel leal <joel.lasi@hotmail.com>
2021-11-10 15:20:00 -0300

    4.3.1 alteração do enuciado

--- exerc-logic43.js
+++ exerc-logic43.js
@@ -1,6 +1,6 @@
 //lista de exercícios do dia 4.3
 
-//Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1,
+//Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 ,
 //imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
 
 let n = 10
