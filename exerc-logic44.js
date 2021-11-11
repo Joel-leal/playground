fe436f7db2470db03f9ed6b61449e6a67e61bcb7
joel leal <joel.lasi@hotmail.com>
2021-11-11 00:52:07 -0300

    exercise 4.4.2

--- exerc-logic44.js
+++ exerc-logic44.js
@@ -6,4 +6,9 @@ let info = {
 
 //1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
 
-console.log(`Bem-vina, ${info.personagem}`)
+// console.log(`Bem-vina, ${info.personagem}`
+
+// 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
+
+info["recorrente"] = 'sim'
+console.log(info)
