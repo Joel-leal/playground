28607ef8a36756b9a9e8360f90e8e9c7c164a4c4
joel leal <joel.lasi@hotmail.com>
2021-11-11 00:37:19 -0300

    Gabarito geral 4.3

--- exerc-logic43.js
+++ exerc-logic43.js
@@ -3,45 +3,45 @@
 // 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 ,
 // imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
 
-// let n = 10
-// let symbol = '*'
-// let listSymbol = ''
-// for(let i = 0; i < 10; i++){
-//   listSymbol += symbol
-// }
-// for(let k = 0; k < 10; k++){
-//   console.log(listSymbol)
-// }
+let t = 10
+let symbol = '*'
+let listSymbol = ''
+for(let i = 0; i < t; i++){
+  listSymbol += symbol
+}
+for(let k = 0; k < 10; k++){
+  console.log(listSymbol)
+}
 
 // 2-Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
 
-// let n = 5
-// let symbol = '*'
-// let listSymbol = ''
-// for(let i = 0; i < 5; i++){
-//   listSymbol += symbol
-//   console.log(listSymbol)
-// }
+let n = 5
+let symbol = '*'
+let listSymbol = ''
+for(let i = 0; i < 5; i++){
+  listSymbol += symbol
+  console.log(listSymbol)
+}
 
 // 3- Agora inverta o lado do triângulo.
 
-// let n = 5 // número de elementos na lista
-// let symbol = '*'
-// let listSymbol = ''
-// let k = n //número de posição de elementos na lista
-// for(let i = 0; i < 5; i++){
-//   for(let p = 0; p < n; p++){
-//     if(p < k){
-//       listSymbol = listSymbol + ' '
-//     }
-//     else(
-//       listSymbol+=symbol
-//     )
-//   }
-//   console.log(listSymbol)
-//   listSymbol = ''
-//   k -=1
-// }
+let n = 5 // número de elementos na lista
+let symbol = '*'
+let listSymbol = ''
+let k = n //número de posição de elementos na lista
+for(let i = 0; i < 5; i++){
+  for(let p = 0; p < n; p++){
+    if(p < k){
+      listSymbol = listSymbol + ' '
+    }
+    else(
+      listSymbol+=symbol
+    )
+  }
+  console.log(listSymbol)
+  listSymbol = ''
+  k -=1
+}
 
 // 4- Depois, faça uma pirâmide com n asteriscos de base:
 
@@ -66,3 +66,5 @@ for(let i = 0; i < k; i++){
   positionRight+=1
   console.log(listSymbol)
 }
+
+// é necessário substituir as variáveis para cada escopo
